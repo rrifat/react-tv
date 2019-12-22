693ab78eedf50f75dacb12b27a83fa199f8a047f
rrifat <rrifat.alam.93@gmail.com>
2019-12-22 11:43:29 +0600

    Fix optional pagination list rendering

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -4,18 +4,19 @@ function Pagination({ pages, handlePageChange, currentPage }) {
   return (
     <nav aria-label="Page navigation example" style={{ cursor: 'pointer' }}>
       <ul className="pagination pagination-lg justify-content-center">
-        {pages.map((page, index) => (
-          <li
-            className="page-item page-link"
-            key={index + page}
-            onClick={() => handlePageChange(page)}
-            style={{
-              backgroundColor: currentPage === page ? '#dee2e6' : '#fff'
-            }}
-          >
-            {page}
-          </li>
-        ))}
+        {pages.length > 1 &&
+          pages.map((page, index) => (
+            <li
+              className="page-item page-link"
+              key={index + page}
+              onClick={() => handlePageChange(page)}
+              style={{
+                backgroundColor: currentPage === page ? '#dee2e6' : '#fff'
+              }}
+            >
+              {page}
+            </li>
+          ))}
       </ul>
     </nav>
   );
